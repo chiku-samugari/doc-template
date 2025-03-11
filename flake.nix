f65e6e795c67dc79eacba0e0b138d920c2d48cdc
chiku <samugari.penguin@gmail.com>
2025-03-12 01:07:25 +0900

    Make it be a flake.

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Collection of Document templates";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      packages.x86_64-linux.default = pkgs.callPackage ./default.nix { };
+    };
+}
